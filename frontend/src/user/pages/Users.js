8eb05e1625a5b7ded35209cd000004cdb5236829
ELATTARIYassine <yassineattari64@gmail.com>
2020-10-07 20:44:24 +0000

    [frontend] feat: UI completed

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,23 +1,18 @@
-import React from "react";
-import UsersList from "../components/UsersList";
+import React from 'react';
+
+import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-      id: "1",
-      name: "Yassine",
+      id: 'u1',
+      name: 'Max Schwarz',
       image:
-        "https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70",
-      places: 3,
-    },
-    {
-      id: "2",
-      name: "Ahmed",
-      image:
-        "https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg",
-      places: 1,
-    },
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
   ];
+
   return <UsersList items={USERS} />;
 };
 
