4f90974ce8053b1b16f507f086388a5f412b6979
ELATTARIYassine <yassineattari64@gmail.com>
2020-10-07 13:19:57 +0000

    [frontend] feat: working on UI wip 45%

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "1",
+      name: "Yassine",
+      image:
+        "https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70",
+      places: 3,
+    },
+    {
+      id: "2",
+      name: "Ahmed",
+      image:
+        "https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg",
+      places: 1,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
